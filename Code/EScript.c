f737f84704f7eed9ec4e9b114ee0ecb11acd560e
eap314 <51955928+eap314@users.noreply.github.com>
2021-05-19 12:45:03 -0400

    Update EScript.c

--- Code/EScript.c
+++ Code/EScript.c
@@ -2,8 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 
+//lines and characters
 int lin=1;
 int cha=0;
+//simple strchr
+int cc(const char* s, int c){if(strchr(s,c)>0){return 1;}return 0;}
+//errors
 int e=0;
 int err(){
 	if(e==0){return 0;}
@@ -12,46 +16,63 @@ int err(){
 	if(e==2){printf("File fail");}
 	if(e==3){printf("Invalid token after '\\'");}
 	return 1;
-}
+}//parse EScript file
 void esparse(char* f){
 	FILE* es=fopen(f, "rt");
 	if(es==NULL){e=2;err();return;}
 	int ch=0;
 	int str=0;
 	int s=0;
+	int dc=0;
+	int var=0;
+	char* tvar="";
 	while(1){
-		ch=fgetc(es);
+		ch=fgetc(es); //get character
 		if(ch==EOF){break;}
-		if(strchr("\"", ch)>0){
+		//string check
+		if(cc("\"", ch)){
 			if(str==0){str=1;}else if(str==1){str=0;}
-		}if(strchr("\'", ch)>0){
+		}if(cc("\'", ch)){
 			if(str==0){str=2;}else if(str==2){str=0;}
-		}if(strchr("\n", ch)>0){lin++;cha=0;continue;}
-		if((strchr("\t ", ch)>0)&&str==0){continue;}
+		}if(cc("\n", ch)){lin++;cha=0;continue;}
+		if((cc("\t ", ch))&&str==0){continue;}
+		//escape check
 		if(s==1){
-			if(strchr("n", ch)>0){
+			if(cc("n", ch)){
 				ch=(int)'\n';
-			}else if(strchr("t", ch)>0){
+			}else if(cc("t", ch)){
 				ch=(int)'\t';
-			}else if(strchr("\\", ch)>0){
+			}else if(cc("\\", ch)){
 				ch=(int)'\\';
 			}else{e=3;}s=0;
 		}
-		if(strchr("\\", ch)>0){s=1;continue;}
-		if(strchr("QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm", ch)>0){
-		}if(strchr("$", ch)>0){
+		if(cc("\\", ch)){s=1;continue;}
+		//letter check
+		if(cc("QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm", ch)){
+			//tvar append ch
+			//var decl
+			//var=0
+		}if(cc("$", ch)){
+			var=1;
 		}//math
-		if(strchr("1234567890", ch)>0){
-		}if(strchr("+", ch)>0){
-		}if(strchr("-", ch)>0){
-		}if(strchr("*", ch)>0){
-		}if(strchr("/", ch)>0){
+		if(cc("1234567890", ch)){
+		}if(cc(".", ch)){
+			dc=1;
+		}if(cc("+", ch)){
+		}if(cc("-", ch)){
+		}if(cc("*", ch)){
+		}if(cc("/", ch)){
 		}
 		cha++;if(err()==1){break;}
 		printf("%c", ch);
 	}
 	return;}
-int main(void){
-	esparse("/users/eap/desktop/C+C/offline/3d/3d.es");
+int main(int argc, char *argv[]){
+	//argument 1 is argv[1]
+	//program name is argv[0]
+
+	//if(strcmp(argv[1], "es")==0){esparse(argv[2]);}
+	esparse("/users/eap/desktop/c+c/offline/3d/3d.es");
+	printf("\n");
 	return 0;
 }
